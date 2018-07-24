e3fa1e91a203e32545bf6cd5adc3686dff9e74ea
kristalys <kristalys.cba4@gmail.com>
2018-07-24 14:04:52 -0400

    Login Works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule} from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { AuthloggedProvider } from '../providers/authlogged/authlogged';
+import { LoginProvider } from '../providers/login/login';
+
+import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthloggedProvider,
+    LoginProvider
   ]
 })
 export class AppModule {}
